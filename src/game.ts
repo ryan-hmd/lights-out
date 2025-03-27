c914fec1165d6ba485fcd7d1fff1ee8fee302037
ryanhmd <ryanhmd.pro@gmail.com>
2025-03-28 00:25:56 +0100

    feat: add changing state function

---
+++ src/game.ts
@@ -0,0 +1,23 @@
+type Board = number[][];
+
+/**
+ * Change the state of a case and its adjacent in a given board.
+ */
+function changeState(board: Board, i: number, j: number): Board {
+    const directions = [
+        [0, 1],
+        [0, -1],
+        [1, 0],
+        [-1, 0],
+    ];
+
+    for (const [dx, dy] of directions) {
+        const x = i + dx;
+        const y = j + dy;
+        if (board[x] && board[x][y]) {
+            board[x][y] = (board[x][y] + 1) % 2;
+        }
+    }
+
+    return board;
+}
