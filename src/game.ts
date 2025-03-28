43072cee7d8f952e29bfabe6df0c31639ec23543
ryanhmd <ryanhmd.pro@gmail.com>
2025-03-28 01:03:48 +0100

    feat: add shuffle function

--- src/game.ts
+++ src/game.ts
@@ -1,6 +1,19 @@
 type Board = number[][];
 
 /**
+ * Shuffle a given board a given number of times.
+ */
+function shuffle(board: Board, shuffles: number): Board {
+    for (let k = 1; k <= shuffles; k++) {
+        const i = Math.floor(Math.random() * board.length);
+        const j = Math.floor(Math.random() * board[0].length);
+        board = changeState(board, i, j);
+    }
+
+    return board;
+}
+
+/**
  * Change the state of a case and its adjacent in a given board.
  */
 function changeState(board: Board, i: number, j: number): Board {
