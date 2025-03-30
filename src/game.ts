2757ab676c90e57a7a88cb0a9a1edb4582487074
ryanhmd <ryanhmd.pro@gmail.com>
2025-03-30 04:10:37 +0200

    feat: add game controllers

---
+++ src/game.ts
@@ -0,0 +1,11 @@
+const settings = document.querySelector("#settings") as HTMLFormElement;
+const gameContainer = document.querySelector("#game") as HTMLDivElement;
+const engine = new Engine();
+
+settings.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const data = new FormData(e.target as HTMLFormElement);
+    const [dimX, dimY] = [data.get("dimX"), data.get("dimY")];
+    const board = new Board(dimX, dimY);
+    engine.to(gameContainer).render(board);
+});
